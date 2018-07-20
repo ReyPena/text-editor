d9e29fd5f69cd2d32fcefe1b640527c6e7b4bcd2
ReyPena <zonickblack@gmail.com>
2018-07-20 13:39:34 -0600

    Implement bus message system

--- src/main/java/TextEditorApp.java
+++ src/main/java/TextEditorApp.java
@@ -1,5 +1,31 @@
-public class TextEditorApp {
+import groovy.lang.Singleton;
+import services.MainService;
+import textAreaComponent.TextAreaComponent;
+
+import javax.swing.*;
+
+@Singleton
+public class TextEditorApp extends JFrame {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        TextEditorApp frame = new TextEditorApp();
+
+        frame.setVisible(true);
     }
+
+    private TextEditorApp() {
+        init();
+
+
+        setBounds(100,100,687,586);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public void init() {
+        MainService.INSTANCE.eventBus.register(new TextChangeHandler());
+
+        TextAreaComponent textArea = new TextAreaComponent();
+        add(textArea);
+    }
+
 }
--- src/main/java/textArea/TextArea.java
+++
@@ -1,4 +0,0 @@
-package textArea;
-
-public class TextArea {
-}
